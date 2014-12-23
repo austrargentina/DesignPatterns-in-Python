c15e0b7acd37502d4287819ae27aed1ab04857cb
austrargentina <dbracher@student.tgm.ac.at>
2014-12-23 17:34:26 +0100

    Finished AdapterDoc

--- DesignPatterns/Adapter/Adapter.py
+++ DesignPatterns/Adapter/Adapter.py
@@ -22,27 +22,49 @@ class IBuch(object,metaclass = ABCMeta):
         pass
 
 class Roman(IBuch):
+    """
+    Klasse Roman, die den Adapter darstellt, da sie das Mittelstück zwischen der 'alten' Klasse
+    Legacy Roman und dem neuen Kunden ist
+    """
 
     def create(self, numWords):
+        """
+        Rechnet die übergebenen Wörter in Seiten um, um so die 'alte' Klasse zu speisen.
+        :param numWords: Anzahl der geschriebenen Wörter
+        :return:
+        """
         LegacyRoman.create(self, numWords/1000) #1000 Wörter pro Seite
 
     def publish(self, booktype):
+        """
+        Setzt anhand des Buchtyps das gewünschte Cover, um so die 'alte' Klasse zu speisen.
+        :param booktype: Art des Buches (Thriller, Horror)
+        :return:
+        """
         if booktype is "Horror":
             LegacyRoman.publish(self, "Hardcover")
         elif booktype is "Thriller":
             LegacyRoman.publish(self, "Softcover")
 
 class LegacyRoman(object):
+    """
+    Adaptierte Klasse LegacyRoman, die nach 'altem' Prinzip funktioniert.
+    """
 
     def create(self, numPages):
         """
-        Test
-        :param numPages:
+        Erzeugt einen Roman, mit einer bestimmten Anzahl an Seiten.
+        :param numPages: Anzahl der Seiten
         :return:
         """
         print("Ein Buch mit %d Seiten wurde erstellt" %numPages)
 
     def publish(self, coverArt):
+        """
+        Published einen Roman, mit einer bestimmten Coverart.
+        :param coverArt:
+        :return:
+        """
         print("Das Buch wurde mit einem %s gepublished" %coverArt)
 
 x = Roman();
